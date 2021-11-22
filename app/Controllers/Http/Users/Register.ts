6fec2c8409d8645c5cf86deeeae23c78074a29a7
Waliston Belles <walistonbelles1@gmail.com>
2021-11-21 22:21:16 -0300

    [feat] alterado formato de keys de retorno da api de snake_case para camelCase

--- app/Controllers/Http/Users/Register.ts
+++ app/Controllers/Http/Users/Register.ts
@@ -27,16 +27,17 @@ export default class UserRegisterController {
         message.to(email)
         message.from('contato@facebook.com', 'Facebook')
         message.subject('Criação de Conta')
-        message.htmlView('emails/register', { link })
+        message.htmlView('emails/verify-email', { link })
       })
     });
   }
 
   public async show({ params }: HttpContextContract) {
     const userKey = await UserKey.findByOrFail('key', params.key);
-    const user = await userKey.related('user').query().firstOrFail();
 
-    return user;
+    await userKey.load('user');
+
+    return userKey.user;
   }
 
   public async update({ request, response }: HttpContextContract) {
