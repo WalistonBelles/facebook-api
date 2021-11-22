4ebf5fdc8096f7409f79eba8592cbf9d7e213b08
Waliston Belles <walistonbelles1@gmail.com>
2021-11-22 14:02:04 -0300

    [feat] adicionado upload de arquivos e registro de avatar do usuario

--- README.md
+++ README.md
@@ -8,11 +8,13 @@ Install [Docker Compose](https://docs.docker.com/compose/install/).
 # Create container with MySQL
 $ docker-compose up -d
 
-# Create database structure
-$ node ace migration:run
+# Create your .env file using .env.example as an example
 
 # install dependencies
 $ npm install
+
+# Create database structure
+$ node ace migration:run
 
 # server with changes watcher
 $ npm run dev
--- env.ts
+++ env.ts
@@ -5,11 +5,12 @@ export default Env.rules({
   PORT: Env.schema.number(),
   APP_KEY: Env.schema.string(),
   APP_NAME: Env.schema.string(),
+  APP_URL: Env.schema.string(),
   DRIVE_DISK: Env.schema.enum(['local'] as const),
   NODE_ENV: Env.schema.enum(['development', 'production', 'testing'] as const),
 
   DB_CONNECTION: Env.schema.string(),
-  
+
   MYSQL_HOST: Env.schema.string({ format: 'host' }),
   MYSQL_PORT: Env.schema.number(),
   MYSQL_USER: Env.schema.string(),
