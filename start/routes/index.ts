1433151b362469dcd433a5fab5c6a232ee4b844c
Waliston Belles <walistonbelles1@gmail.com>
2021-11-22 23:03:06 -0300

    [feat] adicionado websocket o para chat em tempo real entre dois usuários

--- start/routes/index.ts
+++ start/routes/index.ts
@@ -1,18 +1,18 @@
 import Route from '@ioc:Adonis/Core/Route'
-import './auth';
-import './users';
-import './uploads';
-import './posts';
-import './comments';
-import './reactions';
-import './follows';
-import './follows';
-import './profiles';
+import './auth'
+import './users'
+import './uploads'
+import './posts'
+import './comments'
+import './reactions'
+import './follows'
+import './profiles'
+import './messages'
+import './conversations'
 
 Route.get('/', async () => {
   return { hello: 'world' }
-});
-
-Route.get('/user-register', async ({ view }) => {
-  return view.render('emails/register');
 })
+
+Route.on('/test').render('test')
+Route.on('/chat').render('chat')
